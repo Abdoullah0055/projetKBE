4b3e251ee6b2ffd1e6427fd01f27f06202e62415
Abdoullah0055 <elahmarabdou06@gmail.com>
2026-03-18 09:33:47 -0400

    Update AlgosBD.php

--- AlgosBD.php
+++ AlgosBD.php
@@ -1,26 +1,27 @@
+<?php
 function get_pdo()
 {
-$host = '127.0.0.1'; // 127.0.0.1 si la BD et l'application sont sur le même serveur
-$db = 'usager20'; // nom de la base de données
-$user = 'usager20';
-$pass = '4AgGNTNs783E';
-$charset = 'utf8';
+    $host = '127.0.0.1'; // 127.0.0.1 si la BD et l'application sont sur le même serveur
+    $db = 'usager20'; // nom de la base de données
+    $user = 'usager20';
+    $pass = '4AgGNTNs783E';
+    $charset = 'utf8';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-$options = [
-PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-PDO::ATTR_EMULATE_PREPARES => false,
-];
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
 
-try {
-$pdo = new PDO($dsn, $user, $pass, $options);
-// echo "Connexion établie";
-} catch (\PDOException $e) {
-$pdo = false;
-die("ERREUR PAS REUSSI A SE CONNECTER AU PDO");
-//throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-return $pdo;
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+        // echo "Connexion établie";
+    } catch (\PDOException $e) {
+        $pdo = false;
+        die("ERREUR PAS REUSSI A SE CONNECTER AU PDO");
+        //throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+    return $pdo;
 }
