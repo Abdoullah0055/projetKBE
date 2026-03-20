95b6467a5fa9dce5c402a867546661087562d93e
Abdoullah0055 <elahmarabdou06@gmail.com>
2026-03-20 08:53:38 -0400

    Tous les css dans le head

--- test.php
+++ test.php
@@ -1,3 +1,3 @@
 <?php
 include './AlgosBD.php';
-add_item('Test Item', 'This is a test item.', 100, 200, 300, 10, 1, 1);
+add_item('Test ItemOFFICIEL!!!!!!!!!', 'This is a test item.', 100, 200, 300, 10, 1, 1);
