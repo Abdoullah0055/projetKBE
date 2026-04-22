000a51ac42895a8a003124ceac8b0a3e5d188db6
Abdoullah0055 <elahmarabdou06@gmail.com>
2026-04-22 10:18:18 -0400

    bon gt_pdo()

--- AlgosBD.php
+++ AlgosBD.php
@@ -2,24 +2,31 @@
 
 function get_pdo()
 {
-    $host = 'localhost';
-    $port = 8889;
-    $db = 'projetKBE';
-    $user = 'root';
-    $pass = 'root';
-    $charset = 'utf8mb4';
+    static $pdo = null;
 
-    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+    if ($pdo === null) {
+        $host = '127.0.0.1';
+        $db = 'projetKBE';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8mb4';
 
-    try {
-        return new PDO($dsn, $user, $pass, [
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+        $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
-        ]);
-    } catch (\PDOException $e) {
-        die("Erreur PDO : " . $e->getMessage());
+        ];
+
+        try {
+            $pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+            die("Erreur connexion DB");
+        }
     }
+
+    return $pdo;
 }
 
 /**
