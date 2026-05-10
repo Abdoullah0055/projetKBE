0b81393c23b579481dfaf8f1e28e60da9761c9e3
Abdoullah0055 <elahmarabdou06@gmail.com>
2026-05-09 21:42:03 -0400

    accents

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,4 @@
 <footer>
-    L'Arsenal de Sombre-Donjon Â© 2026 | Aventuriers connectÃ©s : 124
+    L'Arsenal de Sombre-Donjon
     <span id="easter-egg" style="cursor: pointer; opacity: 0.1; padding: 0 10px;">.</span>
 </footer>
-
